3499159270315cb27d323e61d63d67805a9b9f46
Jason Miller <Jason@example.com>
2025-08-16 15:59:58 -0700

    almost done, need to add books

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 from stats import get_book_text
 from stats import get_char_count
+from stats import print_report
+import sys
+
+has_book = len(sys.argv[0:-1]) # 1 if arg input, 0 if no arg
+    
+if has_book != 1:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)    
+else:
+    book = sys.argv[-1]
+
+    print(book)
+
+# OK.. I think i just replace with open to book and download the required books.
+    
+
 
 
 def main():
-    get_book_text()
-    get_char_count()
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()    
+    
+    a = get_book_text(file_contents)
+    b = get_char_count(file_contents)
+    c = print_report(a, b)
 
 
 main()
--- stats.py
+++ stats.py
@@ -1,26 +1,70 @@
 
-def get_book_text():
+def get_book_text(file_contents):
     list_words = []
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        list_words = file_contents.split()
-        num_words = 0
-        print(f"{len(list_words)} words found in the document")
+    #with open("books/frankenstein.txt") as f:
+     #   file_contents = f.read()
+    list_words = file_contents.split()
+    #num_words = 0
+    return f"Found {len(list_words)} total words"
 
-def get_char_count():
+def get_char_count(file_contents):
     chars = {}
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        low = file_contents.lower()
+    #with open("books/frankenstein.txt") as f:
+    #    file_contents = f.read()
+    low = file_contents.lower()
         #print(low)
-        cnt = 0
-        for l in low:
-            cnt += 1 
-            if l not in chars:
-                chars[l] = 1
-            else:
-                chars[l] += 1
-        print(chars)
+    cnt = 0
+    for l in low:
+        cnt += 1 
+        if l not in chars:
+            chars[l] = 1
+        else:
+            chars[l] += 1
+    #print(chars)
+    return chars
 
-#get_char_count()
+
+
+def print_report(a, b):
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(a)
+    print(" --------- Character Count -------")
+    
+    sorted_list = []
+    for i in b:
+        val = b[i]
+        sorted_list.append({"char": i, "num": val})
+    
+    def sort_on(sorted_list):
+        return sorted_list["num"]
+
+
+
+    sorted_list.sort(reverse=True, key=sort_on)
+    
+    for i in sorted_list:
+        if str.isalpha(i["char"]):
+            print(f"{i['char']}: {i['num']}")
+
+                  
+    
+    
+    
+    
+    #b.sort(reverse=True, key=sort_on)
+    
+    
+    #for char in chars:
+     #   print(char)
+
+
+
+
+
 #get_book_text()
+#get_char_count()
+#print_report()
+
+
